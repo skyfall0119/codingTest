8920c3bc44bca77ebb526b28841773b7e7ffef40
김성재 <jaykim.coding@gmail.com>
2024-08-15 12:15:05 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 14.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,4 +1,4 @@
-def solution(jobs) :
+def solution(jobs:list) :
     
     curTime = 0
     totalWaitTime = 0
@@ -19,20 +19,20 @@ def solution(jobs) :
             
         ## 현재 시간 기준 처리할수 있는 작업 체크
         ## 작업들 중 제일 짧은 작업 찾기
-        shortTime = -1
+        shortTime = float('inf')
         jobInd = -1
         for i in range(len(jobs)-1, -1,-1) :    
             if jobs[i][0] > curTime :
                 break
-            if shortTime == -1 or (shortTime != -1 and shortTime > jobs[i][1]):
+            if shortTime > jobs[i][1]:
                 shortTime = jobs[i][1]
                 jobInd = i
 
-        # 가장 짧은 작업 pop
+        # 가장 짧은 작업 pop.
+        # 작업에 대한 시간 처리.
         processed = jobs.pop(jobInd)
-
-        print(processed)
         curTime += processed[1]
         totalWaitTime += curTime - processed[0]
     
     return totalWaitTime // numJobs
+    
