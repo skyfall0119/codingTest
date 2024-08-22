bda53bfc3bd1c85cc13415e81ccd34633edcd39e
김성재 <jaykim.coding@gmail.com>
2024-08-22 19:31:52 +0900

    [level 3] Title: 야근 지수, Time: 89.52 ms, Memory: 28.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,32 +1,26 @@
 import numpy as np
 def solution(n, works) :
-    max_val = max(works)
-    arr = np.zeros((max_val, len(works)), dtype=int)
-    for i, num in enumerate(works) :
-        arr[:num, i] = 1
 
-    
-    ind = max_val-1
+    works.sort(reverse=True)
+    arr = np.array(works)
+    total = arr.sum()-n
 
-    for ind in range(max_val-1, -1, -1):
-        # 제일 긴 시간?? 부터 체크
-        
-        indSum = arr[ind].sum()
-        
-        # 작업 시간 남음
-        if indSum <= n :
-            arr[ind].fill(0)
-            n -= indSum
-            
-        # 작업시간 끝
-        else :
-            # 1 인 인덱스 찾기. n개만 사용
-            indices = np.where(arr[ind] == 1)[0][:n]
-            
-            # 1 중에 남은 작업수만큼 0으로 변환
-            for i in indices :
-                arr[ind][i] = 0
-            break
+    if total <= 0 :
+        return 0
+    else :
+        lastInd = len(arr)-1
+        while True :
 
-    ans = int(np.sum(np.square(np.sum(arr, axis=0))))
-    return ans
+            if arr[lastInd] == 0 :
+                lastInd -= 1
+
+            else :
+                if total > lastInd+1 :
+                    
+                    arr[:lastInd+1] -=1
+                    total -= lastInd+1
+                else :
+                    arr[:total] -= 1
+                    break
+                    
+    return int(np.sum( np.square(np.array(works) - arr)))
