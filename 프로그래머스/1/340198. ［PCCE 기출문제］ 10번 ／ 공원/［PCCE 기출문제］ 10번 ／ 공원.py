a065aa104becfbcfd0c50294dc06fbe41843de96
김성재 <jaykim.coding@gmail.com>
2024-11-23 20:05:34 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 17.60 ms, Memory: 28.4 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -2,22 +2,17 @@ import numpy as np
 
 def solution(mats, park):
     answer = -1
-    ## 큰 돗자리부터.
+    
+    ## 큰 돗자리순
     mats.sort(reverse=True)
-    ## numpy
+    ## numpy 변환
     park = np.array(park)
     
     for mat in mats :
-
         for i in range(0, park.shape[0]-mat+1):
             for j in range(0,park.shape[1]-mat+1) :
-                # 돗자리 subarray
-                sub_matrix = park[i:i+mat, j:j+mat]
-                # 알파벳 있으면 패스.
-                if np.any(np.char.isalpha(sub_matrix)):
-                    continue  
-                # 알파벳 없으면 리턴.
-                else :
+                # 돗자리 자리의 모든 인덱스가 -1 이면 리턴.
+                if np.all(park[i:i+mat, j:j+mat] == "-1") :
                     return mat
                     
     return answer
