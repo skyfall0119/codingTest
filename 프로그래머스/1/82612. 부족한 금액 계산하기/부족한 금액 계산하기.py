4e2a1252d041c2296f5f8248375a754ebac9db5a
김성재 <jaykim.coding@gmail.com>
2025-04-22 15:50:56 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.30 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,10 @@
+def solution(price, money, count):
+    
+    total = 0
+    for i in range(1, count+1):
+        total += price * i
+    
+    if money >= total:
+        return 0
+    else:
+        return total - money
