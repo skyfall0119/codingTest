f317ec5f5097575a4c9af23f49ae5d58fd77e9b0
김성재 <jaykim.coding@gmail.com>
2025-05-22 23:43:01 +0900

    [level 2] Title: 괄호 변환, Time: 0.43 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,43 @@
+def split_uv(w):
+    left, right = 0, 0
+    u, v = "", ""
+    for i, ch in enumerate(w):
+        if ch == "(":
+            left += 1
+        else:
+            right += 1
+            
+        if left == right:
+            split_idx = min(i+1, len(w))
+            return w[:split_idx], w[split_idx:]
+    
+    return w, ""
+
+def correct_p(s):
+    stack = []
+
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False  
+            stack.pop() 
+
+    return len(stack) == 0  
+            
+
+
+def solution(p):
+    if p == "":
+        return ""
+    
+    u,v = split_uv(p)
+    
+    if correct_p(u):
+        return u + solution(v)
+
+    else:
+        tmp = "(" + solution(v) + ")"
+        flip = u[1:-1].translate(str.maketrans("()" , ")("))
+        return tmp + flip
