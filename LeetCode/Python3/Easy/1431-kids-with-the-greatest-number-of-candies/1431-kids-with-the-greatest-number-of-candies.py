42b1f1bcb48b853be72de1b6e43d77ff25cffc46
김성재 <jaykim.coding@gmail.com>
2025-12-18 00:34:46 +0900

    Time: 0 ms (100%), Space: 29.2 MB (12.33%) - LeetHub

---
+++ LeetCode/Python3/Easy/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,9 @@
+import numpy as np
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+
+        # numpy
+        return ((np.array(candies) + extraCandies) >= np.max(candies)).tolist()
+
+        # list comp
+        # return [c + extraCandies >= max(candies) for c in candies]
