b34bbce5878150bf63ecbf9f70cf2840735029e8
김성재 <jaykim.coding@gmail.com>
2025-12-17 20:48:52 +0900

    Time: 3 ms (93.63%), Space: 18.7 MB (95.22%) - LeetHub

--- LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.py
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,35 +1,28 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        ans = []
-        temp = [] # going left asters.
-
-        while asteroids:
-            one = asteroids.pop()
-            if one > 0: # check temp for rightside
-                if not temp: # farmost right. will never collide
-                    ans.append(one)
-                    continue
-
-                while temp:
-                    if abs(temp[-1]) > abs(one): # one lose
-                        break
-                    
-                    elif abs(temp[-1]) < abs(one): # one win
-                        temp.pop()
-                        continue
-                    # tie
-                    temp.pop()
-                    break
-                else:
-                    # one remains
-                    ans.append(one)
-            else: # goin left
-                temp.append(one)
         
-        temp.reverse()
-        ans.reverse()
-        temp.extend(ans)
+        stack = []
+        for ast in asteroids:
+            if ast < 0:
+                if not stack: # farleft. discard
+                    stack.append(ast)
+                    continue
+                
+                while stack and stack[-1] > 0: 
+                    if abs(stack[-1]) > abs(ast):
+                        break
+                    if abs(stack[-1]) == abs(ast):
+                        stack.pop()
+                        break
+                    # ast bigger
+                    stack.pop()
+                    
+                else:
+                    stack.append(ast)
+            
+            else:
+                stack.append(ast)
 
-        return temp
+        return stack
         
 
