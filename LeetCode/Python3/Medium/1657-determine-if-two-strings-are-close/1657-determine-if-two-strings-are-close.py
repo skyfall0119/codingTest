70bed1f7faf572c7cb91c8ddc030d997741a8dba
김성재 <jaykim.coding@gmail.com>
2025-12-20 00:46:33 +0900

    Time: 87 ms (67.91%), Space: 18.2 MB (75.68%) - LeetHub

--- LeetCode/Python3/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ LeetCode/Python3/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -2,12 +2,13 @@ from collections import Counter
 
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        w1, w2 = Counter(word1), Counter(word2)
-        w1v, w2v = w1.values(), w2.values()
 
         # different length
-        if sum(w1v) != sum(w2v):
+        if len(word1) != len(word2):
             return False
+
+        w1, w2 = Counter(word1), Counter(word2)
+        w1v, w2v = w1.values(), w2.values()
 
         # should be made of same character
         for k1, k2 in zip(w1.keys(), w2.keys()):
