65117d42c48aed4950245a3e8c18e72e8813e390
김성재 <jaykim.coding@gmail.com>
2025-12-20 00:47:24 +0900

    Time: 87 ms (67.91%), Space: 18.3 MB (21.51%) - LeetHub

--- LeetCode/Python3/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ LeetCode/Python3/Medium/1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -8,15 +8,13 @@ class Solution:
             return False
 
         w1, w2 = Counter(word1), Counter(word2)
-        w1v, w2v = w1.values(), w2.values()
 
         # should be made of same character
-        for k1, k2 in zip(w1.keys(), w2.keys()):
-            if k1 not in w2 or k2 not in w1:
-                return False
+        if set(w1.keys()) != set(w2.keys()):
+            return False
 
         # number counts don't match
-        for a,b in zip(sorted(w1v), sorted(w2v)):
+        for a,b in zip(sorted(w1.values()), sorted(w2.values())):
             if a != b:
                 return False
 
