7b42839d2a70c155855da0151375aed900a7ac97
김성재 <jaykim.coding@gmail.com>
2026-01-04 19:04:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Python3/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ LeetCode/Python3/Medium/2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -10,14 +10,14 @@ class Solution:
         if head is None:
             return 0
 
-        q = deque([head.val])
+        q = deque(head)
         while head.next:
             head = head.next
-            q.append(head.val)
+            q.append(head)
 
         max_sum = 0
         while q:
-            temp_sum = q.popleft() + q.pop()
+            temp_sum = q.popleft().val + q.pop().val
             max_sum = max(temp_sum, max_sum)
         
         return max_sum
